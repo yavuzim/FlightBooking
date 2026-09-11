a97c23844db5f4b17e6c14bc7fb93f0e48f7808e
yavuzim <kyavuzimer@gmail.com>
2026-09-11 12:03:18 +0300

    edition createbooking page

---
+++ FlightBooking/Areas/Admin/Controllers/BookingController.cs
@@ -0,0 +1,32 @@
+﻿using FlightBooking.Services.FlightServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BookingController : Controller
+    {
+        private readonly IFlightService _flightService;
+        public BookingController(IFlightService flightService)
+        {
+            _flightService = flightService;
+            
+        }
+        public async Task<IActionResult> CreateBooking(string id)
+        {
+            var value = await _flightService.GetFlightByIdAsync(id);
+            ViewBag.FlightNumber = value.FlightNumber;
+            ViewBag.DepartureAirportCode = value.DepartureAirportCode;
+            ViewBag.DepartureAirportName = value.DepartureAirportName;
+            ViewBag.ArrivalAirportCode = value.ArrivalAirportCode;
+            ViewBag.ArrivalAirportName = value.ArrivalAirportName;
+            ViewBag.DepartureTime = value.DepartureTime;
+            ViewBag.ArrivalTime = value.ArrivalTime;
+            return View();
+        }
+        public IActionResult BookingList()
+        {
+            return View();
+        }
+    }
+}
